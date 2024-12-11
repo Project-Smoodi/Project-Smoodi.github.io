38b6e9dadf6377f5d4fd8f57b29b8383b916bfb7
Daybreak312 <ty82afg12@gmail.com>
2024-12-12 08:48:28 +0900

    UPDATE :: tech -> blog

--- tag/header/script.js
+++ tag/header/script.js
@@ -1,13 +1,13 @@
 import {changeLanguage} from "/language.js";
 
 export function loadHeader() {
-    document.querySelector("#change-language").addEventListener("click", event => {
+    document.querySelector("#change-language").addEventListener("click", () => {
         changeLanguage()
     })
 
     if (location.pathname.includes("docs/")) {
         document.getElementsByClassName("header-logo-text")[0].innerHTML = "Project Smoodi <b>Docs</b>";
-    } else if (location.pathname.includes("tech/")) {
+    } else if (location.pathname.includes("blog/")) {
         document.getElementsByClassName("header-logo-text")[0].innerHTML = "Project Smoodi <b>Tech<b/> Blog";
     } else {
         document.getElementsByClassName("header-logo-text")[0].textContent = "Project Smoodi";
